de947b222ced9e6b7b0f5581a2efc5e2114215b5
Dante Elrik <dante@elrikgroup.com>
2017-01-04 14:02:05 -0500

    Solution 0

--- https:ZZwww.hackerrank.comZchallengesZmini-max-sum.hs
+++ https:ZZwww.hackerrank.comZchallengesZmini-max-sum.hs
@@ -1,24 +1,32 @@
-module HackerrankxcomZchallengesZminizmaxzsum where
+--module HackerrankxcomZchallengesZminizmaxzsum where
+module Main where
 import Control.Monad
-nums = [1,2,3,4,5]
-out  = [10,14]
+import System.IO
 
 ignore :: Integer -> [Integer] -> [Integer]
-ignore x q = do 
-  y <- q 
-  guard (x /= y)
-  return y
+ignore = go False
+  where
+    go False _ []     = []
+    go False x (y:ys) = if x == y then go True x ys else y : go False x ys
+    go True  _ ys     = ys
 
-input = foldr (\int out -> ignore int nums : out ) [] nums
+inputs raw = foldr (\int out -> ignore int raw : out ) [] raw
 
 data MaxMin  = MaxMin {
   mm_max :: Integer,
   mm_min :: Integer
-} deriving Show
+}
+instance Show MaxMin where show (MaxMin max min) = show min ++ " " ++ show max
 
 mm_empty init = MaxMin init init
 
-answer = foldl go zed input
+answer raw = foldl go zed raw' 
   where
-    go (MaxMin max' min') set = let sum' = sum set in MaxMin (max max' sum') (min min' sum')
-    zed  = mm_empty (sum $ head input)
+    go (MaxMin max' min') set = 
+      let sum' = sum set 
+      in MaxMin (max max' sum') (min min' sum')
+    zed  = mm_empty (sum $ head raw')
+    raw' = (inputs raw)
+
+main :: IO ()
+main = (fmap (fmap read . words) getLine :: IO [Integer]) >>= putStrLn . show . answer 
