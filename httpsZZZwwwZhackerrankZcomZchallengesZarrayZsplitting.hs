e2fbbe0c297181047b2dc2051a498ba0e715e19c
Dante Haskell Elrik <dante@elrikgroup.com>
2017-01-08 18:27:08 -0500

    Solution 6 replaces linked lists with Data.Vector.Unboxed -- still times out ~_~

--- httpsZZZwwwZhackerrankZcomZchallengesZarrayZsplitting.hs
+++ httpsZZZwwwZhackerrankZcomZchallengesZarrayZsplitting.hs
@@ -1,17 +1,29 @@
 {-# LANGUAGE EmptyDataDecls,BangPatterns #-}
 module Main where -- httpsZZZwwwZhackerrankZcomZchallengesZarrayZsplitting.hs
-import Prelude hiding (partition)
+import Prelude hiding (partition, (!))
 import Data.Maybe
 import Control.Monad
-array        = [2, 2, 2, 2]
-array2       = [1,0, 0, 1]
-array3       = [4, 1, 0, 1, 1, 0, 1]
-array4       = [3,3,3]
-partition (x:y:[])
-  | x == y    = Just ([x],[y])
-  | otherwise = Nothing
-partition [x]  = Nothing
-partition (xs) = go (0, []) (length xs -1, [])
+import Data.Vector.Unboxed ((!))
+import qualified Data.Vector.Unboxed as Data.Vector.Unboxed
+
+array,array2,array3,array4 :: Data.Vector.Unboxed.Vector Int
+array        = Data.Vector.Unboxed.fromList [2, 2, 2, 2]
+array2       = Data.Vector.Unboxed.fromList [1,0, 0, 1]
+array3       = Data.Vector.Unboxed.fromList [4, 1, 0, 1, 1, 0, 1]
+array4       = Data.Vector.Unboxed.fromList [3,3,3]
+
+partition xs
+  | Data.Vector.Unboxed.length xs == 1 = Nothing
+  | Data.Vector.Unboxed.length xs == 2 = 
+    let 
+      x = xs ! 0
+      y = xs ! 1
+    in case x == y of
+      True  ->  Just (Data.Vector.Unboxed.singleton x, Data.Vector.Unboxed.singleton y)
+      False ->  Nothing
+  | otherwise                          = go 
+                                         (0                               , Data.Vector.Unboxed.empty) 
+                                         (Data.Vector.Unboxed.length xs -1, Data.Vector.Unboxed.empty)
   where go (!i, !ys) (!j, !zs)
           | i > j                = 
             case rweight == lweight of
@@ -19,24 +31,24 @@ partition (xs) = go (0, []) (length xs -1, [])
             False -> Nothing
           | rweight' == lweight' = 
             case (compare y z) of 
-              GT -> go (i , ys)        (j + joff, zs')
-              EQ -> go (i, ys)         (j + joff, zs')
-              LT -> go (i + ioff, ys') (j, zs)
-          | lweight' > rweight'  = go (i, ys)         (j + joff, zs')
-          | rweight' > lweight'  = go (i + ioff, ys') (j,zs)
+              GT ->                go (i       , ys)  (j + joff, zs')
+              EQ ->                go (i       , ys)  (j + joff, zs')
+              LT ->                go (i + ioff, ys') (j       , zs)
+          | lweight' > rweight'  = go (i       , ys)  (j + joff, zs')
+          | rweight' > lweight'  = go (i + ioff, ys') (j       , zs)
           where
-            y       = xs !! i
-            z       = xs !! j
-            ys'     = ys ++ [y]
-            zs'     = z:zs
+            y       = xs ! i
+            z       = xs ! j
+            ys'     = Data.Vector.Unboxed.snoc ys y
+            zs'     = Data.Vector.Unboxed.cons z zs
             ioff
               | lweight' > rweight' = 0
               | otherwise           = 1
             joff
               | rweight' > lweight' = 0
               | otherwise           = -1
-            (lweight,lweight') = (sum ys, sum ys')
-            (rweight,rweight') = (sum zs, sum zs')
+            (lweight,lweight') = (Data.Vector.Unboxed.sum ys, Data.Vector.Unboxed.sum ys')
+            (rweight,rweight') = (Data.Vector.Unboxed.sum zs, Data.Vector.Unboxed.sum zs')
 
 score = go 0
   where 
@@ -49,5 +61,5 @@ main = do
   cnt <- fmap (read :: String -> Int) getLine
   forM_ [1..cnt] $ \_ -> do
     len   <- fmap (read :: String -> Int) getLine
-    ints  <- fmap (fmap read . words) getLine
-    print $ score ints
+    ints  <- fmap (fmap read . words) getLine :: IO [Int]
+    print $ score (Data.Vector.Unboxed.fromList ints)
