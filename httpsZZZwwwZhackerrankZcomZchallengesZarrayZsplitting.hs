e242a654f28da948de6605d934bc0b077eeeb6e9
Dante Haskell Elrik <dante@elrikgroup.com>
2017-01-08 02:48:32 -0500

    Solution 6 - WREQd

--- httpsZZZwwwZhackerrankZcomZchallengesZarrayZsplitting.hs
+++ httpsZZZwwwZhackerrankZcomZchallengesZarrayZsplitting.hs
@@ -1,25 +1,33 @@
 {-# LANGUAGE EmptyDataDecls,BangPatterns #-}
 module Main where -- httpsZZZwwwZhackerrankZcomZchallengesZarrayZsplitting.hs
-import System.IO.Unsafe (unsafePerformIO)
 import Prelude hiding (partition)
 import Data.Maybe
+import Control.Monad
 array        = [2, 2, 2, 2]
 array2       = [1,0, 0, 1]
 array3       = [4, 1, 0, 1, 1, 0, 1]
+array4       = [3,3,3]
+partition (x:y:[])
+  | x == y    = Just ([x],[y])
+  | otherwise = Nothing
 partition [x]  = Nothing
 partition (xs) = go (0, []) (length xs -1, [])
-  where go !(!i, !ys) !(!j, !zs)
+  where go (!i, !ys) (!j, !zs)
           | i > j                = 
             case rweight == lweight of
             True  -> Just (ys, zs)
             False -> Nothing
-          | rweight' == lweight' = go (i + ioff, ys') (j + joff, zs')
+          | rweight' == lweight' = 
+            case (compare y z) of 
+              GT -> go (i , ys)        (j + joff, zs')
+              EQ -> go (i, ys)         (j + joff, zs')
+              LT -> go (i + ioff, ys') (j, zs)
           | lweight' > rweight'  = go (i, ys)         (j + joff, zs')
           | rweight' > lweight'  = go (i + ioff, ys') (j,zs)
           where
             y       = xs !! i
             z       = xs !! j
-            ys'     = y:ys
+            ys'     = ys ++ [y]
             zs'     = z:zs
             ioff
               | lweight' > rweight' = 0
@@ -30,10 +38,16 @@ partition (xs) = go (0, []) (length xs -1, [])
             (lweight,lweight') = (sum ys, sum ys')
             (rweight,rweight') = (sum zs, sum zs')
 
+score = go 0
+  where 
+    go score xs =
+      case partition xs  of 
+      Just (left, right) -> max (go (succ score) left) (go (succ score) right)
+      Nothing            -> score
 
-            
-          
-
-  
-
-  
+main = do
+  cnt <- fmap (read :: String -> Int) getLine
+  forM_ [1..cnt] $ \_ -> do
+    len   <- fmap (read :: String -> Int) getLine
+    ints  <- fmap (fmap read . words) getLine
+    print $ score ints
